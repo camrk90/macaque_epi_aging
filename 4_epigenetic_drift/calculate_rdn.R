6311a98f7b81e93f56d5aafd8c8e55cb793c4c1c
Cam Kelsey <ckelsey4@asu.edu>
2025-06-24 21:12:36 -0700

    update rdn script

--- 4_epigenetic_drift/calculate_rdn.R
+++ 4_epigenetic_drift/calculate_rdn.R
@@ -1,28 +1,12 @@
+#!/usr/bin/env /packages/apps/spack/18/opt/spack/gcc-11.2.0/r-4.2.2-kpl/bin/Rscript
+
 library(tidyverse)
 library(stringr)
 library(readr)
 
-#Import kinship matrix----------------------------------------------------------
-kinship<- readRDS("/scratch/ckelsey4/Cayo_meth/full_kin_matrix")
-
-#Subset and rearrange kinship rows and cols to match metadata
-kinship<- kinship[long_data$lid_pid, long_data$lid_pid]
-
-#Import metadata----------------------------------------------------------------
-long_data<- readRDS("/scratch/ckelsey4/Cayo_meth/long_data_adjusted")
-long_data<- long_data %>%
-  dplyr::rename(sex = individual_sex) %>%
-  filter(n > 1) %>%
-  arrange(lid_pid)
-
-lid_pid<- long_data$lid_pid
-lid_pid<- paste(lid_pid, "_meanPDN.bed", sep = "")
-
 ##set unique file ID's
 unique_fileID <- list.files(path = "/scratch/ckelsey4/Cayo_meth/epigenetic_drift/avg_pdn/",
                             pattern = "*_meanPDN.bed")
-
-unique_fileID<- unique_fileID[unique_fileID %in% lid_pid]
 
 ##set file paths
 filePaths <- paste0("/scratch/ckelsey4/Cayo_meth/epigenetic_drift/avg_pdn/", unique_fileID)
@@ -30,10 +14,9 @@ filePaths <- paste0("/scratch/ckelsey4/Cayo_meth/epigenetic_drift/avg_pdn/", uni
 ##loop to calculate PDN
 avg_pdn<- data.frame(matrix(nrow=190910, ncol=0))
 read_cov<- data.frame(matrix(nrow=190910, ncol=0))
-overlap<- read.table(filePaths[1], header = FALSE, fill = TRUE)
 
-for(i in 1:5){
-  if (i == 1){
+for(i in 1:length(unique_fileID)){
+  if (i == 1) {
     print(i)
     #Read in file
     overlap<- read.table(filePaths[i], header = FALSE, fill = TRUE)
@@ -45,8 +28,8 @@ for(i in 1:5){
     overlap<- overlap %>%
       relocate(region, .before = chrom)
     
-    pdn<- overlap[c(1, 6)]
-    cov<- overlap[c(1,5)]
+    pdn<- overlap[c(1:2, 6)]
+    cov<- overlap[c(1:2,5)]
     
     #Generate unique id
     id<- paste(unique_fileID[i])
@@ -79,6 +62,9 @@ for(i in 1:5){
     read_cov<- cbind(read_cov, cov)
     print(paste(i, "done!", sep = " "))
   }
+  rm(overlap)
+  rm(cov)
+  rm(pdn)
 }
 
 #Convert periods to NA
@@ -89,56 +75,25 @@ avg_pdn[avg_pdn == "."]<- NA
 avg_pdn<- drop_na(avg_pdn) #lme4 drops rows with NAs anyway so this code just removes them ahead of time
 read_cov<- read_cov[read_cov$region %in% avg_pdn$region,]
 
+chrs<- str_sort(c(1:20, "X"), numeric = TRUE)
+
 rownames(avg_pdn)<- avg_pdn$region
 avg_pdn<- avg_pdn %>%
-  select(-region) %>%
-  t()
+  mutate(chrom = factor(chrom, levels = chrs)) %>%
+  arrange(chrom) %>%
+  select(-region)
+avg_pdn_list<- split(avg_pdn, avg_pdn$chrom)
 
 rownames(read_cov)<- read_cov$region
 read_cov<- read_cov %>%
-  select(-region) %>%
-  t()
+  mutate(chrom = factor(chrom, levels = chrs)) %>%
+  arrange(chrom) %>%
+  select(-region)
+read_cov_list<- split(read_cov, read_cov$chrom)
 
-write.csv(compiled, file="/scratch/ckelsey4/Cayo_meth/epigenetic_drift/mean_pdn_all.csv")
+saveRDS(avg_pdn_list, "/scratch/ckelsey4/Cayo_meth/epigenetic_drift/avg_pdn/avg_pdn_list")
+saveRDS(read_cov_list, "/scratch/ckelsey4/Cayo_meth/epigenetic_drift/avg_pdn/read_cov_list")
 
-#Model Vectors for lme4---------------------------------------------------------
-cov<- regions_cov[[SAMP]]
-meth<- regions_m[[SAMP]]
-
-###################################
-#####        Run PQLseq       #####
-###################################
-#Run PQLseq for within_age-------------------------------------------------------------
-#Generate model matrix
-predictor_matrix<- model.matrix(~ within.age + mean.age + individual_sex + university, data = long_data)
-w.age_phenotype<- predictor_matrix[, 2]
-w.age_covariates<- predictor_matrix[, 3:4]
-
-#Run pqlseq model
-w.age_pqlseq2_model<- pqlseq2(Y = meth, x = w.age_phenotype, 
-                              K = kinship, W = w.age_covariates, 
-                              lib_size = cov, model="BMM")
-
-#Run PQLseq for mean_age-------------------------------------------------------------
-#Generate model matrix
-m.age_phenotype<- predictor_matrix[, 3]
-m.age_covariates<- predictor_matrix[, c(2,4)]
-
-#Run pqlseq model
-m.age_pqlseq2_model<- pqlseq2(Y = meth, x = m.age_phenotype, 
-                              K = kinship, W = m.age_covariates, 
-                              lib_size = cov, model="BMM")
-
-#Run PQLseq for sex-------------------------------------------------------------
-#Generate model matrix
-predictor_matrix<- model.matrix(~ within.age + mean.age + individual_sex + university, data = long_data)
-sex_phenotype<- predictor_matrix[, 4]
-sex_covariates<- predictor_matrix[, c(2:3)]
-
-#Run pqlseq model
-sex_pqlseq2_model<- pqlseq2(Y = meth, x = sex_phenotype, 
-                            K = kinship, W = sex_covariates, 
-                            lib_size = cov, model="BMM")
 
 
 
