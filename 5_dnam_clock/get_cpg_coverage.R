9ef605bfc1a42cf62b6682dcbd391f1987b4cfaa
Cam Kelsey <ckelsey4@asu.edu>
2025-07-11 14:34:07 -0700

    update dnam_clock scripts

--- 5_dnam_clock/get_cpg_coverage.R
+++ 5_dnam_clock/get_cpg_coverage.R
@@ -1,35 +1,28 @@
 #!/usr/bin/env /packages/apps/spack/18/opt/spack/gcc-11.2.0/r-4.2.2-kpl/bin/Rscript
 
+SAMP <- Sys.getenv("SLURM_ARRAY_TASK_ID")
+SAMP <- as.integer(SAMP)
+
 library(bsseq)
 
 #Load in data-------------------------------------------------------------------
 cayo_filtered_list<- readRDS("/scratch/ckelsey4/Cayo_meth/cayo_filtered_list")
-cayo_filtered_list<- cayo_filtered_list[1:21]
+cayo_filtered<- cayo_filtered_list[[SAMP]]
 
 #Generate coverage for individual CpGs
-cpg_m_list<- parallel::mclapply(names(cayo_filtered_list),function(x){
-  dd=getCoverage(cayo_filtered_list[[x]], regions = NULL, type = "M", 
-                 what = "perBase")
-  rownames(dd)<- cayo_filtered_list[[x]]@rowRanges@ranges@start
-  return(as.data.frame(dd))
-}, mc.cores=21)
+#M count
+m=getCoverage(cayo_filtered, regions = NULL, type = "M", what = "perBase")
+rownames(m)<- cayo_filtered@rowRanges@ranges@start
+m<- as.data.frame(m)
 
-names(cpg_m_list)<- 1:21
-cpg_m<- do.call(rbind, cpg_m_list)
-rm(cpg_m_list)
+#Coverage
+cov=getCoverage(cayo_filtered, regions = NULL, type = "Cov", what = "perBase")
+rownames(cov)<- cayo_filtered@rowRanges@ranges@start
+cov<- as.data.frame(cov)
 
-cpg_cov_list<- parallel::mclapply(names(cayo_filtered_list),function(x){
-  dd=getCoverage(cayo_filtered_list[[x]], regions = NULL, type = "Cov", 
-                 what = "perBase")
-  rownames(dd)<- cayo_filtered_list[[x]]@rowRanges@ranges@start
-  return(as.data.frame(dd))
-}, mc.cores=21)
+file_path<- paste("/scratch/ckelsey4/Cayo_meth/dnam_clock/", SAMP, sep="")
 
-names(cpg_cov_list)<- 1:21
-cpg_cov<- do.call(rbind, cpg_cov_list)
-rm(cpg_cov_list)
-
-write.table(cpg_m, "/scratch/ckelsey4/Cayo_meth/dnam_clock/cpg_m.txt", quote = F)
-
-write.table(cpg_cov, "/scratch/ckelsey4/Cayo_meth/dnam_clock/cpg_cov.txt", quote = F)
+#Write files as txt
+write.table(m, paste(file_path, "cpg_m", "txt", sep="."), quote = F)
+write.table(cov, paste(file_path, "cpg_cov", "txt", sep="."), quote = F)
 
